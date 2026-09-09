6a9acf9691482195826e2e707fcd48b607cbfbe2
Himanshu Choudhary <baliyanh10@gmail.com>
2026-09-09 16:57:21 +0530

    running react by parcel

--- episode 1/app.js
+++ episode 1/app.js
@@ -1,40 +1,28 @@
-// By using React
-// const heading = React.createElement("h1", { id: "heading" }, "Hello, React! from React by using CDN");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// nested elements
-// <div id="parent">
-// <div id="child1">
-// h1>Hello, React! from React by using CDN</h1>
-// </div>
-// <div id="child2">
-// h1>Hello, React! from React by using CDN</h1>
-// </div>
-// </div>
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
+const parent = React.createElement("div", { id: "parent", key: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
     React.createElement(
       "h1",
-      {},
+      {key: "h1-child1"},
       "Hello, h1! from React by using Parent to child1 element",
     ),
     React.createElement(
       "h2",
-      {},
+      {key: "h2-child1"},
       "Hello, h2! from React by using Parent to child element",
     ),
   ]),
-  React.createElement("div", { id: "child2" }, [
+  React.createElement("div", { id: "child2", key: "child2" }, [
     React.createElement(
       "h1",
-      {},
+      {key: "h1-child2"},
       "Hello, h1! from React by using Parent to child2 element",
     ),
     React.createElement(
       "h2",
-      {},
+      {key: "h2-child2"},
       "Hello, h2! from React by using Parent to child element",
     ),
   ]),
